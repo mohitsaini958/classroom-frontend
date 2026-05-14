5764b52a6f3b3fb2fc946003edc8ebc9b3009272
Mohit Saini <mohitsaini9581@gmail.com>
2026-05-14 13:36:10 +0530

    implement data provider

---
+++ src/Constants/index.ts
@@ -0,0 +1,12 @@
+export const DEPARTMENTS =[
+    'CS',
+    'Math',
+    'English',
+];
+
+export const DEPARTMENTS_OPTIONS = DEPARTMENTS.map((item) => (
+    {
+        value: item,
+        label: item,
+    }
+))
